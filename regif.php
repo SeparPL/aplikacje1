ec70b772978d00dbe2ff4cdf53c1d732a65d06d9
SeparPL <szymon.nieciag17@gmail.com>
2023-10-10 14:38:32 +0200

    regif

--- regif.php
+++
@@ -1,8 +0,0 @@
-<?php
-$wzorzec='/\S*\.gif/i';
-$tekst="w pliku obrazek.gif jest ikona";
-if(preg_match($wzorzec,$tekst))
-    echo "znaleziono obrazek";
-else
-    echo "nie znaleziono obrazka";
-?>
